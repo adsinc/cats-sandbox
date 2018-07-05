7abf8e8089f2f0b198731694d4d07cbe32607dff
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-05 18:59:42 +0300

    Printable

--- src/main/scala/sandbox/PrintableLib.scala
+++ src/main/scala/sandbox/PrintableLib.scala
@@ -9,16 +9,14 @@ class PrintableLib {
   object Printable {
     def format[A](a: A)(implicit printable: Printable[A]): String =
       printable.format(a)
-
     def print[A](a: A)(implicit printable: Printable[A]): Unit =
       println(format(a))
   }
 
   object PrintableInstances {
-
     implicit val intPrintableInstance: Printable[Int] = (a: Int) => a.toString
-
     implicit val stringPrintableInstance: Printable[String] = (a: String) => a.toString
-
   }
+
+  final case class Cat(name: String, any: Int, color: String)
 }
