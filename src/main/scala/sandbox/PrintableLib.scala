9013a4db78167faf435d0b1617bdedaedc323e59
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-05 18:58:15 +0300

    Printable

--- src/main/scala/sandbox/PrintableLib.scala
+++ src/main/scala/sandbox/PrintableLib.scala
@@ -2,4 +2,23 @@ package sandbox
 
 class PrintableLib {
 
+  trait Printable[A] {
+    def format(a: A): String
+  }
+
+  object Printable {
+    def format[A](a: A)(implicit printable: Printable[A]): String =
+      printable.format(a)
+
+    def print[A](a: A)(implicit printable: Printable[A]): Unit =
+      println(format(a))
+  }
+
+  object PrintableInstances {
+
+    implicit val intPrintableInstance: Printable[Int] = (a: Int) => a.toString
+
+    implicit val stringPrintableInstance: Printable[String] = (a: String) => a.toString
+
+  }
 }
