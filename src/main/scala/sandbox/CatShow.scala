84da38533b11d78599048688b62e930694e0008d
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-12 18:05:42 +0300

    Show

--- src/main/scala/sandbox/CatShow.scala
+++ src/main/scala/sandbox/CatShow.scala
@@ -1,7 +1,11 @@
 package sandbox
 
+import cats._
+
 object CatShow {
 
   final case class Cat(name: String, age: Int, color: String)
 
+  implicit val catShow: Show[Cat] =
+    Show.show(cat => s"${cat.name} is a ${cat.age} year-old ${cat.color} cat.")
 }
