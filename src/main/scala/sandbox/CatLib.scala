e95551edaddc8dc0b468709b4eccdc8b012dfb3f
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-12 20:00:02 +0300

    Eq

--- src/main/scala/sandbox/CatLib.scala
+++ src/main/scala/sandbox/CatLib.scala
@@ -2,6 +2,7 @@ package sandbox
 
 import cats._
 import cats.implicits._
+import sandbox.CatLib.Cat
 
 object CatLib {
 
@@ -17,5 +18,13 @@ object CatLib {
 }
 
 object CatTest extends App {
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
 
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  println(cat1 === cat2)
+  println(optionCat1 === Some(cat1))
+  println(optionCat1 =!= optionCat2)
 }
