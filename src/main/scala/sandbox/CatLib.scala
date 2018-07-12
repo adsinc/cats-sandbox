3ab47af0336ca9d015dc8a34d4f4a2d9cd2ab338
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-12 19:56:07 +0300

    Eq

--- src/main/scala/sandbox/CatLib.scala
+++ src/main/scala/sandbox/CatLib.scala
@@ -3,10 +3,19 @@ package sandbox
 import cats._
 import cats.implicits._
 
-object CatShow {
+object CatLib {
 
   final case class Cat(name: String, age: Int, color: String)
 
   implicit val catShow: Show[Cat] =
     Show.show(cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat.")
+
+  implicit val catEq: Eq[Cat] =
+    Eq.apply { (cat1, cat2) =>
+      cat1.name === cat2.name && cat1.age == cat2.age && cat1.color == cat2.color
+    }
+}
+
+object CatTest extends App {
+
 }
