9a98504f885a108594c361184d31c3b0d46a250b
adsinc <sinc86@gmail.com>
2018-07-22 02:44:48 +0300

    writer

---
+++ src/main/scala/sandbox/Writers.scala
@@ -0,0 +1,35 @@
+package sandbox
+
+import sandbox.Writers.factorial
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationDouble
+import scala.util.Random
+import cats._
+import cats.data.Writer
+import cats.implicits._
+
+object Writers {
+  def slowly[A](body: => A): A =
+    try body finally Thread.sleep(Random.nextInt(50) + 50)
+
+  type Logged[T] = Writer[Vector[String], T]
+
+  def factorial(n: Int): Logged[Int] = {
+    for {
+      ans <- if (n == 0) 1.pure[Logged]
+             else slowly(factorial(n - 1).map(_ * n))
+      _ <- Vector(s"fact $n $ans").tell
+    } yield ans
+  }
+}
+
+object WriterTest extends App {
+  val r = Await.result(Future.sequence(Vector(
+    Future(factorial(5)),
+    Future(factorial(5))
+  )), 5.seconds)
+
+  println(r)
+}
