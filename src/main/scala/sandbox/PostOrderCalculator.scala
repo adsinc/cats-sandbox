28909c020b1cc0aba9a258ae8706357d5299da02
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-23 19:37:46 +0300

    state

--- src/main/scala/sandbox/PostOrderCalculator.scala
+++ src/main/scala/sandbox/PostOrderCalculator.scala
@@ -1,6 +1,7 @@
 package sandbox
 
 import cats.data.State
+import sandbox.PostOrderCalculator._
 
 object PostOrderCalculator {
   type CalcState[A] = State[List[Int], A]
@@ -17,13 +18,29 @@ object PostOrderCalculator {
   def op(fn: (Int, Int) => Int): CalcState[Int] =
     State { s =>
       val a :: b :: newState = s
-      (newState, fn(a, b))
+      val value = fn(a, b)
+      (value :: newState, fn(a, b))
     }
 
-  def evalAll() = ???
+  def evalAll(input: List[String]): CalcState[Int] = {
+    input.foldLeft(State.pure[List[Int], Int](0)) {
+      (state, sym) => state.flatMap(_ => evalOne(sym))
+    }
+  }
+
+  def evalInput(s: String): Int = ???
 
 }
 
 object PostOrderCalculatorRunner extends App {
+  val program1 = evalAll(List("1", "2", "+", "3", "*"))
+  println(program1.run(Nil).value)
 
+  val program2 = for {
+    _ <- evalAll(List("1", "2", "+"))
+    _ <- evalAll(List("3", "4", "+"))
+    ans <- evalOne("*")
+  } yield ans
+
+  println(program2.run(Nil).value)
 }
