3eac0320fb35445d19939c8b0e38b65122a8e288
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-07-24 16:10:30 +0300

    tree

---
+++ src/main/scala/sandbox/TreeMonad.scala
@@ -0,0 +1,52 @@
+package sandbox
+
+import cats._
+import cats.implicits._
+
+object TreeMonad {
+
+  sealed trait Tree[+A]
+
+  final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  final case class Leaf[A](value: A) extends Tree[A]
+
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = Branch(left, right)
+
+  def leaf[A](value: A): Tree[A] = Leaf(value)
+
+  implicit val treeMonad: Monad[Tree] = new Monad[Tree] {
+    override def pure[A](x: A): Tree[A] = leaf(x)
+
+    override def flatMap[A, B](fa: Tree[A])(f: A => Tree[B]): Tree[B] =
+      fa match {
+        case Branch(l, r) => branch(flatMap(l)(f), flatMap(r)(f))
+        case Leaf(a) => f(a)
+      }
+
+    //    @tailrec
+    override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] =
+      f(a) match {
+        case Branch(left, right) =>
+          for {
+            l <- left
+            r <- right
+          } yield branch(tailRecM(l)(f), tailRecM(r)(f))
+        case Leaf(Left(value)) => tailRecM(value)(f)
+        case Leaf(Right(value)) => leaf(value)
+      }
+  }
+}
+
+object TreeMonadTest extends App {
+
+  import TreeMonad._
+
+  val tree = branch(leaf(1), leaf(2))
+
+  val r = for {
+    t <- tree
+  } yield t * 2
+
+  println(r)
+}
