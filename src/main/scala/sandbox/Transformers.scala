73694a1de0bc1acdcf37266126286da736f31bc0
adsinc <sinc86@gmail.com>
2018-07-26 23:17:59 +0300

    transformers

--- src/main/scala/sandbox/Transformers.scala
+++ src/main/scala/sandbox/Transformers.scala
@@ -3,6 +3,7 @@ package sandbox
 import cats.data.EitherT
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Transformers extends App {
 
@@ -14,7 +15,11 @@ object Transformers extends App {
     "Hot Rod" -> 10
   )
 
-  def getPowerLevel(autobot: String): Response[Int] = {
-    powerLevels.get(autobot)
-  }
+  def getPowerLevel(autobot: String): Response[Int] =
+    powerLevels.get(autobot) match {
+      case Some(value) => EitherT[Future, String, Int](Future(Right(value)))
+      case None => EitherT[Future, String, Int](Future(Left(autobot)))
+    }
+
+
 }
