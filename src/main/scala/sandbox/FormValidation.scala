e09c1b1b6af0747b6d64f5acc5c1d86c43ffe8d8
adsinc <sinc86@gmail.com>
2018-07-30 23:47:27 +0300

    form validate

---
+++ src/main/scala/sandbox/FormValidation.scala
@@ -0,0 +1,44 @@
+package sandbox
+
+import cats._
+import cats.data._
+import cats.implicits._
+
+object FormValidation {
+
+  type FormData = Map[String, String]
+  type FailFast[A] = Either[List[String], A]
+  type FailSlow[A] = Validated[List[String], A]
+
+  def getValue(data: FormData)(valueName: String): FailFast[String] =
+    data.get(valueName)
+      .toRight(List(s"No value $valueName"))
+
+  def parseInt(name: String)(value: String): FailFast[Int] =
+    Either.catchOnly[NumberFormatException](value.toInt)
+      .leftMap(_ => List(s"$name must be integer"))
+
+  def nonBlank(name: String)(value: String): FailFast[String] =
+    Either.right(value)
+      .ensure(List(s"$name is empty"))(_.nonEmpty)
+
+  def nonNegative(name: String)(value: Int): FailFast[Int] =
+    Either.right(value)
+      .ensure(List(s"$name can't be negative"))(_ >= 0)
+
+  def readName(data: FormData): FailFast[String] =
+    getValue(data)("name")
+      .flatMap(name => nonBlank("name")(name))
+
+  def readName(data: FormData): FailFast[String] =
+    getValue(data)("name")
+      .flatMap(nonBlank("name"))
+
+  def readAge(data: FormData): FailFast[Int] =
+    getValue(data)("age")
+      .flatMap(nonBlank("age"))
+      .flatMap(parseInt("age"))
+      .flatMap(nonNegative("age"))
+
+}
+
