dc6e01d77b7ff5b204e0448ad00aa2dd42550bd1
adsinc <sinc86@gmail.com>
2018-08-03 23:09:38 +0300

    mapreduce

--- src/main/scala/sandbox/MapReduce.scala
+++ src/main/scala/sandbox/MapReduce.scala
@@ -3,9 +3,28 @@ package sandbox
 import cats._
 import cats.implicits._
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object MapReduce {
 
-  def foldMap[A, B: Monoid](seq: Vector[A])(f: A => B): B =
+  def foldMap_[A, B: Monoid](seq: Vector[A])(f: A => B): B =
     seq.foldLeft(Monoid.empty[B])(_ |+| f(_))
+
+  def parFoldMap_[A, B: Monoid](values: Vector[A])(f: A => B): Future[B] = {
+    val processors = Runtime.getRuntime.availableProcessors()
+    foldMap_(values.grouped(values.length / processors).toVector) { group =>
+      Future(foldMap_(group)(f))
+    }
+  }
+
+  def parFoldMap[A, B: Monoid](values: Vector[A])(f: A => B): Future[B] = {
+    val processors = Runtime.getRuntime.availableProcessors()
+    values
+      .grouped(values.length / processors)
+      .toVector
+      .foldMap(g => Future(g.foldMap(f)))
+  }
+
 
 }
