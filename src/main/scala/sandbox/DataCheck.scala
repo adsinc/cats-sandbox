c537a7045e7ddba64f0bd520edd5f1051cf4c9e7
adsinc <sinc86@gmail.com>
2018-08-07 00:17:25 +0300

    data validation

--- src/main/scala/sandbox/DataCheck.scala
+++ src/main/scala/sandbox/DataCheck.scala
@@ -8,10 +8,25 @@ object DataCheck {
   sealed trait Check[E, A] {
     def and(that: Check[E, A]): Check[E, A] = And(this, that)
 
-    def apply(value: A): Either[E, A]
+    def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] =
+      this match {
+        case Pure(func) =>
+          func(a)
+
+        case And(left, right) =>
+          (left(a), right(a)) match {
+            case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
+            case (Left(e), Right(_)) => e.asLeft
+            case (Right(_), Left(e)) => e.asLeft
+            case (Right(_), Right(_)) => a.asRight
+          }
+      }
   }
 
-  final case class And[E, A](left: Check[E, A], right: Check[E, A]) extends Check[E, A]
+  final case class And[E, A](left: Check[E, A],
+                             right: Check[E, A]) extends Check[E, A]
+
+  final case class Pure[E, A](func: A => Either[E, A]) extends Check[E, A]
 
   final case class CheckF[E: Semigroup, A](func: A => Either[E, A]) {
     def apply(value: A): Either[E, A] =
