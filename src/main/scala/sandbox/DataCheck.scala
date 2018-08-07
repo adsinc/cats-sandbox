899f56895a59eaec5e9f612de7ff7f56d270ffa1
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-08-07 18:29:00 +0300

    data check

--- src/main/scala/sandbox/DataCheck.scala
+++ src/main/scala/sandbox/DataCheck.scala
@@ -1,6 +1,7 @@
 package sandbox
 
 import cats._
+import cats.data.Validated
 import cats.implicits._
 
 object DataCheck {
@@ -8,39 +9,30 @@ object DataCheck {
   sealed trait Check[E, A] {
     def and(that: Check[E, A]): Check[E, A] = And(this, that)
 
-    def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] =
+    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] =
       this match {
         case Pure(func) =>
           func(a)
 
         case And(left, right) =>
-          (left(a), right(a)) match {
-            case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
-            case (Left(e), Right(_)) => e.asLeft
-            case (Right(_), Left(e)) => e.asLeft
-            case (Right(_), Right(_)) => a.asRight
-          }
+          (left(a), right(a)).mapN((_, _) => a)
       }
   }
 
   final case class And[E, A](left: Check[E, A],
                              right: Check[E, A]) extends Check[E, A]
 
-  final case class Pure[E, A](func: A => Either[E, A]) extends Check[E, A]
+  final case class Pure[E, A](func: A => Validated[E, A]) extends Check[E, A]
 
-  final case class CheckF[E: Semigroup, A](func: A => Either[E, A]) {
-    def apply(value: A): Either[E, A] =
-      func(value)
-
-    def and(that: CheckF[E, A]): CheckF[E, A] =
-      CheckF { a =>
-        (this (a), that(a)) match {
-          case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
-          case (Left(e), Right(_)) => e.asLeft
-          case (Right(_), Left(e)) => e.asLeft
-          case (Right(_), Right(_)) => a.asRight
-        }
-      }
+  val a: Check[List[String], Int] = Pure { v =>
+    if (v > 2) v.valid
+    else List("Must be > 2").invalid
   }
 
+  val b: Check[List[String], Int] = Pure { v =>
+    if (v < -2) v.valid
+    else List("Must be < -2").invalid
+  }
+
+  val check: Check[List[String], Int] = a and b
 }
