53ef7267d9fcfcbbab71d1c6c3001cf9a01d53ce
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-08-08 18:57:29 +0300

    data check

--- src/main/scala/sandbox/DataCheck.scala
+++ src/main/scala/sandbox/DataCheck.scala
@@ -6,10 +6,10 @@ import cats.implicits._
 
 object DataCheck {
 
-  sealed trait Check[E, A] {
-    def and(that: Check[E, A]): Check[E, A] = And(this, that)
+  sealed trait Predicate[E, A] {
+    def and(that: Predicate[E, A]): Predicate[E, A] = And(this, that)
 
-    def or(that: Check[E, A]): Check[E, A] = Or(this, that)
+    def or(that: Predicate[E, A]): Predicate[E, A] = Or(this, that)
 
     def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] =
       this match {
@@ -27,28 +27,36 @@ object DataCheck {
       }
   }
 
-  final case class And[E, A](left: Check[E, A],
-                             right: Check[E, A]) extends Check[E, A]
+  final case class And[E, A](left: Predicate[E, A],
+                             right: Predicate[E, A]) extends Predicate[E, A]
 
-  final case class Or[E, A](left: Check[E, A],
-                            right: Check[E, A]) extends Check[E, A]
+  final case class Or[E, A](left: Predicate[E, A],
+                            right: Predicate[E, A]) extends Predicate[E, A]
 
-  final case class Pure[E, A](func: A => Validated[E, A]) extends Check[E, A]
+  final case class Pure[E, A](func: A => Validated[E, A]) extends Predicate[E, A]
 
-  val a: Check[List[String], Int] = Pure { v =>
+  sealed trait Check[E, A, B] {
+    def apply(a: A): Validated[E, B] =
+      ???
+
+    def map[C](func: B => C): Check[E, A, C] =
+      ???
+  }
+
+  val a: Predicate[List[String], Int] = Pure { v =>
     if (v > 2) v.valid
     else List("Must be > 2").invalid
   }
 
-  val b: Check[List[String], Int] = Pure { v =>
+  val b: Predicate[List[String], Int] = Pure { v =>
     if (v < -2) v.valid
     else List("Must be < -2").invalid
   }
 
-  val c: Check[List[String], Int] = Pure { v =>
+  val c: Predicate[List[String], Int] = Pure { v =>
     if (v == 0) v.valid
     else List("Must be = 0").invalid
   }
 
-  val check: Check[List[String], Int] = a and b or c
+  val check: Predicate[List[String], Int] = a and b or c
 }
