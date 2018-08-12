5373da7e98a2cb6619dcfcbb0538e76fb9f483bf
Aleksei Dolgii <sinc86@gmail.com>
2018-08-12 19:16:02 +0300

    CRDT

--- src/main/scala/sandbox/CRDT.scala
+++ src/main/scala/sandbox/CRDT.scala
@@ -5,18 +5,31 @@ import cats.implicits._
 
 object CRDT {
 
-  final case class GCounter[A](counters: Map[String, A]) {
-    def increment(machine: String, amount: A)(implicit m: Monoid[A]): GCounter[A] = {
-      val newAmount = amount |+| counters.getOrElse(machine, m.empty)
-      GCounter(counters + (machine -> newAmount))
-    }
+  trait GCounter[F[_, _], K, V] {
+    def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
 
-    def merge(that: GCounter[A])(implicit b: BoundedSemiLattice[A]): GCounter[A] =
-      GCounter(counters |+| that.counters)
+    def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
 
-    def total(implicit m: Monoid[A]): A =
-      counters.values.toList.combineAll
+    def total(f1: F[K, V])(implicit m: Monoid[V]): V
   }
+
+  object GCounter {
+    def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]): GCounter[F, K, V] = counter
+  }
+
+  implicit def mapGCounter[K, V]: GCounter[Map, K, V] =
+    new GCounter[Map, K, V] {
+      def increment(map: Map[K, V])(k: K, v: V)(implicit m: Monoid[V]): Map[K, V] = {
+        val newV = v |+| map.getOrElse(k, m.empty)
+        map + (k -> newV)
+      }
+
+      def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+        f1 |+| f2
+
+      def total(f1: Map[K, V])(implicit m: Monoid[V]): V =
+        f1.values.toList.combineAll
+    }
 
   trait BoundedSemiLattice[A] extends Monoid[A] {
     def combine(x: A, y: A): A
@@ -42,4 +55,14 @@ object CRDT {
         Set.empty[T]
     }
 
+  trait KeyValueStore[F[_, _]] {
+    def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+
+    def get[K, V](f: F[K, V])(k: K): Option[V]
+
+    def getOrElse[K, V](f: F[K, V])(k: K, default: V): V =
+      get(f)(k).getOrElse(default)
+
+    def values[K, V](f: F[K, V]): List[V]
+  }
 }
